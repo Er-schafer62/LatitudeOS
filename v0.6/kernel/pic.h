434c20a5533b6a891f5f725c29bb03e9616e88e4
Er-schafer62 <Schafer@purelymail.com>
2026-05-25 00:54:27 -0600

    Delete v0.6 directory

--- v0.6/kernel/pic.h
+++
@@ -1,7 +0,0 @@
-#ifndef PIC_H
-#define PIC_H
-
-void pic_init();
-void pic_send_eoi(int irq);
-
-#endif
--- v0.6/kernel/time.h
+++
@@ -1,7 +0,0 @@
-#ifndef TIME_H
-#define TIME_H
-
-/* Print current RTC time to the console */
-void cmd_time(void);
-
-#endif
